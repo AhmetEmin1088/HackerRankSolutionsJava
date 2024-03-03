3486907fa1d550428e7ec1d013271b4df80383e8
AhmetEmin1088 <ahmeteminyilmaz23@gmail.com>
2024-03-03 20:05:48 +0300

    Welcome to Java!

---
+++ welcomeToJava/src/Main.java
@@ -0,0 +1,11 @@
+
+//HackerRank -> Java -> Welcome to Java!
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
